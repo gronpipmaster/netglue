eaaa476f076e6c945ce447ef4561e0ad6ebf4f7e
Gronpipmaster <gronpipmaster@gmail.com>
2014-06-06 00:33:35 +0700

    bug fixed to many open files

--- netglue.go
+++ netglue.go
@@ -32,22 +32,39 @@ type NetGlue struct {
 }
 
 //Create new NetGlue server, listen to InAddr and run sender for OutAddr
-func NewNetGlue(rcvr interface{}, output chan interface{}) (*NetGlue, error) {
-	p := &NetGlue{make(chan *msg, BufferSize), output}
+func NewNetGlue(rcvr interface{}, output chan interface{}) (connect *NetGlue, err error) {
+	connect = &NetGlue{make(chan *msg, BufferSize), output}
 	//construct listen
 	rpc.Register(rcvr)
 	listen, err := net.Listen(Network, InAddr)
 	if err != nil {
-		return nil, err
+		return
 	}
 	if Verbose {
 		log.Println("Listen success", Network, InAddr)
 	}
 	//run listener
-	go p.runListener(listen)
+	go connect.runListener(listen)
+
+	client, err := rpc.Dial(Network, OutAddr)
+	if err != nil {
+		if Verbose {
+			log.Println("Client connect err", err)
+			log.Println("Sleep 5 second and try again.")
+		}
+		time.Sleep(5 * time.Second)
+		client, err = rpc.Dial(Network, OutAddr)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if Verbose {
+		log.Println("Connect success", Network, OutAddr)
+	}
 	//run sender
-	go p.runSender()
-	return p, nil
+	go connect.runSender(client)
+
+	return
 }
 
 func (p *NetGlue) Send(serviceMethod string, args interface{}, reply interface{}) {
@@ -57,20 +74,22 @@ func (p *NetGlue) Send(serviceMethod string, args interface{}, reply interface{}
 }
 
 func (p *NetGlue) runListener(listen net.Listener) {
+	defer listen.Close()
 	for {
 		conn, err := listen.Accept()
 		if err != nil {
 			if Verbose {
-				log.Println(err)
+				log.Println("Accept output - ", err)
 			}
 			//TODO panic/fatal, or error for client this library see rpc.Accept http://golang.org/src/pkg/net/rpc/server.go?s=5911:5935#L595
-			continue
+			break
+		} else {
+			go rpc.ServeConn(conn)
 		}
-		go rpc.ServeConn(conn)
 	}
 }
 
-func (p *NetGlue) runSender() {
+func (p *NetGlue) runSender(client *rpc.Client) {
 	for {
 		select {
 		//waiting msg from self queue
@@ -78,20 +97,20 @@ func (p *NetGlue) runSender() {
 			//connect to OutAddr
 			client, err := rpc.Dial(Network, OutAddr)
 			if err == nil {
-				//send async msg
-				divCall := client.Go(msg.ServiceMethod, msg.Args, msg.Reply, nil)
-				replyMsg := <-divCall.Done // will be equal to divCall
-				if replyMsg.Error != nil && Verbose {
-					log.Println(replyMsg.Error)
+				//send msg
+				err := client.Call(msg.ServiceMethod, msg.Args, msg.Reply)
+				if err != nil && Verbose {
+					log.Println(err)
 				}
 				//send output chanel reply
 				go func() {
-					p.output <- replyMsg.Reply
+					p.output <- msg.Reply
+					client.Close()
 				}()
 			} else {
 				//replay send
 				if Verbose {
-					log.Println("Dialing output", err)
+					log.Println("Dialing output - ", err)
 					log.Println("Sleep 1 second and try again.")
 				}
 				time.Sleep(1 * time.Second)
